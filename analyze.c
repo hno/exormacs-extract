0361fb0369d08a7b2b1528782dea87e4a7cd199c
Henrik Nordström <henrik@hno.se>
2021-12-06 01:31:38 +0100

    Process file sets/directories

--- analyze.c
+++ analyze.c
@@ -30,6 +30,7 @@
  * files crossing several image files
  *
  *   mkdir output
+ *
  *   for file in *.img; do
  *     ./analyze $file output
  *   done
@@ -42,23 +43,38 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-struct __attribute__((__packed__)) dirblock {
-	char unknown1[6];
-	char label[10];
+const char *output_folder = NULL;
+
+struct __attribute__((__packed__)) set_table {
+	char unknown1[0xe];
 	union {
-		char raw[0x342-0x310];
-		struct __attribute__((__packed__)) direntry {
-			char name[12];
-			uint32_t first_block;
-			uint32_t blocks;
+		char raw[0x10];
+		struct __attribute__((__packed__)) set_table_entry {
+			uint32_t valid;
+			char name[8];
+			uint32_t block;
 		} entry;
-	} dir[];
+	} table[];
 };
 
-void read_at(FILE *in, int offset, char *buf, int size)
+struct __attribute__((__packed__)) file_table {
+	char unknown1[6];
+	char setname[10];
+	union {
+		char raw[0x32];
+		struct __attribute__((__packed__)) file_table_entry {
+			char name[12];
+			uint32_t block;
+			uint32_t blocks;
+		} entry;
+	} table[];
+};
+
+void read_at(FILE *in, int start_block, char *buf, int size)
 {
-	fseek(in, offset, SEEK_SET);
+	fseek(in, start_block * 0x100, SEEK_SET);
 	fread(buf, size, 1, in);
 }
 
@@ -71,32 +87,82 @@ void hexdump(const char *label, const char *buf, int size)
 	printf("\n");
 }
 
-void print_dir_entry(struct direntry *entry)
+void trim_spaces(char *name)
 {
-	printf("%-8.8s.%-4.4s ", &entry->name[0], &entry->name[8]);
-	printf(" first_block=%-4d size=%-5d", be32toh(entry->first_block), be32toh(entry->blocks)+1);
+	for (int i = strlen(name) - 1; i >= 0; i--) {
+		if (name[i] != ' ')
+			break;
+		name[i] = 0;
+	}
+}
+
+void print_file_table_entry(const char *set, struct file_table_entry *entry)
+{
+	printf("%s/%-8.8s.%-4.4s ", set, &entry->name[0], &entry->name[8]);
+	printf(" block=%-4d size=%-5d", be32toh(entry->block), be32toh(entry->blocks)+1);
 	printf("\n");
 }
 
-void save_file(FILE *in, const char *set, struct direntry *entry)
+void print_set_table_entry(struct set_table_entry *entry)
+{
+	printf("%-8.8s/ block=%-4d\n", entry->name, be32toh(entry->block));
+}
+
+void save_file(FILE *in, const char *set, struct file_table_entry *entry)
 {
 	char filename[10];
 	char path[32];
 	memcpy(filename, entry->name, 8);
 	filename[8] = 0;
-	for (int i = 7; i > 0; i--) {
-		if (filename[i] != ' ')
-			break;
-		filename[i] = 0;
-	}
-	snprintf(path, sizeof(path), "%s/%s.%s", set, filename, entry->name+8);
+	trim_spaces(filename);
+	// Create output folder structure
+	mkdir(output_folder, ACCESSPERMS);
+	snprintf(path, sizeof(path), "%s/%s", output_folder, set);
+	mkdir(path, ACCESSPERMS);
+	snprintf(path, sizeof(path), "%s/%s/%s.%s", output_folder, set, filename, entry->name+8);
+	// Save file
 	FILE *out = fopen(path, "ab");
 	for (uint32_t file_block = 0; file_block < be32toh(entry->blocks)+1; file_block++) {
 		char buf[0x100];
-		read_at(in, (be32toh(entry->first_block) + file_block) * 0x100, buf, 0x100);
+		read_at(in, (be32toh(entry->block) + file_block) * 0x100, buf, 0x100);
 		fwrite(buf, 0x100, 1, out);
 	}
 	fclose(out);
+}
+
+void read_file_table(FILE *in, char *set, uint32_t start_block)
+{
+	char buf[0x400];
+	read_at(in, start_block, buf, sizeof(buf));
+	struct file_table *table = (void *)buf;
+	//hexdump("block", buf, 0x100);
+	hexdump("unknown", table->unknown1, sizeof(table->unknown1));
+	printf("Set: %.8s\n", table->setname);
+	for (int i = 0; i < 50 && table->table[i].entry.name[0]; i++) {
+		hexdump("file_table_entry", table->table[i].raw, sizeof(table->table[i].raw));
+		struct file_table_entry *entry = &table->table[i].entry;
+		print_file_table_entry(set, entry);
+		save_file(in, set, entry);
+	}
+}
+
+void read_set_table(FILE *in, uint32_t start_block)
+{
+	char buf[0x100];
+	read_at(in, start_block, buf, sizeof(buf));
+	struct set_table *table = (void *)buf;
+	//hexdump("block", buf, 0x100);
+	hexdump("unknown", table->unknown1, sizeof(table->unknown1));
+	for (int i = 0; i < 50 && table->table[i].entry.valid; i++) {
+		hexdump("set_table_entry", table->table[i].raw, sizeof(table->table[i].raw));
+		struct set_table_entry *entry = &table->table[i].entry;
+		print_set_table_entry(entry);
+		char set_name[9];
+		memcpy(set_name, entry->name, sizeof(entry->name));
+		set_name[8] = 0;
+		trim_spaces(set_name);
+		read_file_table(in, set_name, be32toh(entry->block));
+	}
 }
 
 int main(int argc, char **argv)
@@ -105,16 +171,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Usage: %s input.img outputfolder\n", argv[0]);
 		exit(1);
 	}
-	char buf[0x400];
 	FILE *in = fopen(argv[1], "rb");
-	read_at(in, 0x300, buf, sizeof(buf));
-	struct dirblock *dp = (void *)buf;
-	//hexdump("block", buf, 0x100);
-	hexdump("unknown", dp->unknown1, sizeof(dp->unknown1));
-	printf("Label: %.16s\n", dp->label);
-	for (int i = 0; i < 50 && dp->dir[i].entry.name[0]; i++) {
-		hexdump("entry", dp->dir[i].raw, sizeof(dp->dir[i].raw));
-		print_dir_entry(&dp->dir[i].entry);
-		save_file(in, argv[2], &dp->dir[i].entry);
-	}
+	output_folder = argv[2];
+	read_set_table(in, 2);
 }
