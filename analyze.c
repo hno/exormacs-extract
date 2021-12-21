e61fd8f8998d80cf9f96c3d3407a6469254c2837
Henrik Nordström <henrik@hno.se>
2021-12-21 16:20:24 +0100

    Implement more of the actual disk structures
    Only linear files supported. sequential or ISAM files not yet
    implemented.

--- analyze.c
+++ analyze.c
@@ -76,30 +76,48 @@ struct __attribute__((__packed__)) volume_id_block {
 };
 
 struct __attribute__((__packed__)) secondary_directory_block {
-	char unknown1[0x10];
-	union {
-		char raw[0x10];
-		struct __attribute__((__packed__)) secondary_directory_block_entry {
-			uint16_t user_numer;
-			char name[8];
-			uint32_t block;
-			uint8_t reserved[2];
-		} entry;
-	} table[];
+	uint32_t next;
+	char reserved[12];
+	struct __attribute__((__packed__)) secondary_directory_block_entry {
+		uint16_t user_numer;
+		char name[8];
+		uint32_t block;
+		uint8_t reserved[2];
+	} entry[15];
 };
 
 struct __attribute__((__packed__)) primary_directory_block {
-	char unknown1[6];
-	char setname[10];
-	union {
-		char raw[0x32];
-		struct __attribute__((__packed__)) primary_directory_block_entry {
-			char name[12];
-			uint32_t block;
-			uint32_t blocks;
-			char unknown1[30];
-		} entry;
-	} table[];
+	uint32_t next;
+	uint16_t user_number;
+	char catalogue[8];
+	char reserved[2];
+	struct __attribute__((__packed__)) primary_directory_block_entry {
+		char name[10];
+		char reserved1[2];
+		uint32_t start;
+		uint32_t end;
+		uint32_t eof;
+		uint32_t eor;
+		uint8_t write_acess_code;
+		uint8_t read_access_code;
+		uint8_t attributes;
+		uint8_t last_block_size;
+		uint16_t record_size;
+		char reserved2[1];
+		uint8_t key_size;
+		uint8_t fab_size;
+		uint8_t block_size;
+		uint16_t date_created;
+		uint16_t date_assigned;
+		char reserved3[8];
+	} entry[20];
+};
+
+enum {
+	FILE_TYPE_CONTIGUOUS = 0,
+	FILE_TYPE_SEQUENTIAL = 1,
+	FILE_TYPE_ISAM = 2,
+	FILE_TYPE_ISAMD = 3
 };
 
 void read_at(FILE *in, int start_block, char *buf, int size)
@@ -108,13 +126,14 @@ void read_at(FILE *in, int start_block, char *buf, int size)
 	fread(buf, size, 1, in);
 }
 
-void hexdump(const char *buf, int size)
+void hexdump(const void *_buf, int size)
 {
+	const unsigned char *buf = _buf;
 	for (int i = 0; i < size; i++)
-		printf("%s%02X", size > 0 ? " ": "", (unsigned char)buf[i]);
+		printf("%s%02X", size > 0 ? " ": "", buf[i]);
 }
 
-void hexdata(const char *label, const char *buf, int size)
+void hexdata(const char *label, const void *buf, int size)
 {
 	if (label)
 		printf("%s ", label);
@@ -131,16 +150,36 @@ void trim_spaces(char *name)
 	}
 }
 
-void print_primary_directory_block_entry(const char *set, struct primary_directory_block_entry *entry)
+void print_primary_directory_block_entry(const char *catalogue, struct primary_directory_block_entry *entry)
 {
-	printf("%-8s/%-8.8s.%-4.4s ", set, &entry->name[0], &entry->name[8]);
-	printf(" block=%-4d", be32toh(entry->block));
-	if (strlen(set)) // Not sure this is right condition. Maybe should look at the extra data after the filename instead.
-		printf(" size=%-4d", be32toh(entry->blocks)+1);
-	else
-		printf("          ");
-	printf(" ");
-	hexdump(entry->unknown1, sizeof(entry->unknown1));
+	printf("%-8s/%-8.8s.%-4.4s ", catalogue, &entry->name[0], &entry->name[8]);
+	int type = entry->attributes & 0xf;
+	switch(type) {
+	case FILE_TYPE_CONTIGUOUS:
+		printf(" start=%-4d size=%-4d", be32toh(entry->start), be32toh(entry->end)+1);
+		break;
+	case FILE_TYPE_SEQUENTIAL:
+		printf(" sqeuential");
+		int record_size = be16toh(entry->record_size);
+		if (record_size)
+			printf(" record_size=%d", record_size);
+		else
+			printf(" dynamic_record_size");
+		if (verbose) {
+			printf(" ");
+			hexdump(&entry, sizeof(entry));
+		}
+		// TODO: Any additional fields of interest?
+		break;
+	case FILE_TYPE_ISAM:
+	case FILE_TYPE_ISAMD:
+		printf(" ISAM");
+		if (type == FILE_TYPE_ISAMD)
+			printf(" null non-unique");
+		// TODO: Many more fields.
+		// TODO: Dive down into FAB data?
+		break;
+	}
 	printf("\n");
 }
 
@@ -149,7 +188,7 @@ void print_secondary_directory_block_entry(struct secondary_directory_block_entr
 	printf("%-8.8s/               block=%-4d\n", entry->name, be32toh(entry->block));
 }
 
-char *prepare_path(const char *set, struct primary_directory_block_entry *entry)
+char *prepare_path(const char *catalogue, struct primary_directory_block_entry *entry)
 {
 	char filename[10];
 	static char path[32];
@@ -158,62 +197,58 @@ char *prepare_path(const char *set, struct primary_directory_block_entry *entry)
 	trim_spaces(filename);
 	// Create output folder structure
 	mkdir(output_folder, ACCESSPERMS);
-	snprintf(path, sizeof(path), "%s/%s", output_folder, set);
+	snprintf(path, sizeof(path), "%s/%s", output_folder, catalogue);
 	mkdir(path, ACCESSPERMS);
-	snprintf(path, sizeof(path), "%s/%s/%s.%s", output_folder, set, filename, entry->name+8);
+	snprintf(path, sizeof(path), "%s/%s/%s.%s", output_folder, catalogue, filename, entry->name+8);
 	return path;
 }
 
-void save_file(FILE *in, const char *set, struct primary_directory_block_entry *entry)
+void save_file(FILE *in, const char *catalogue, struct primary_directory_block_entry *entry)
 {
-	char *path = prepare_path(set, entry);
+	char *path = prepare_path(catalogue, entry);
 	FILE *out = fopen(path, "ab");
-	uint32_t blocks = be32toh(entry->blocks)+1;
+	uint32_t blocks = be32toh(entry->end)+1;
 	for (uint32_t file_block = 0; file_block < blocks; file_block++) {
 		char buf[0x100];
-		read_at(in, be32toh(entry->block) + file_block, buf, sizeof(buf));
+		read_at(in, be32toh(entry->start) + file_block, buf, sizeof(buf));
 		fwrite(buf, sizeof(buf), 1, out);
 	}
 	fclose(out);
 }
 
-void save_index_file(FILE *in, const char *set, struct primary_directory_block_entry *entry)
-{
-	char *path = prepare_path(set, entry);
-	FILE *out = fopen(path, "ab");
-	char buf[0x100];
-	read_at(in, be32toh(entry->block), buf, sizeof(buf));
-	fwrite(buf, sizeof(buf), 1, out);
-	read_at(in, be32toh(*(uint32_t *)(buf+10)), buf, sizeof(buf));
-	fwrite(buf, sizeof(buf), 1, out);
-	fclose(out);
-}
-
-void read_primary_directory_block(FILE *in, char *set, uint32_t start_block)
+void read_primary_directory_block(FILE *in, char *catalogue, uint32_t start_block)
 {
 	char buf[0x400];
 	read_at(in, start_block, buf, sizeof(buf));
 	struct primary_directory_block *table = (void *)buf;
 	if (debug)
-		hexdata("block", buf, 0x100);
+		hexdata("pdp", buf, 0x100);
 	if (verbose)
-		hexdata("unknown", table->unknown1, sizeof(table->unknown1));
-	if (verbose)
-		printf("Set: %.8s\n", table->setname);
+		printf("Catalogue: %.8s\n", table->catalogue);
 	for (int i = 0; i < 20; i++) {
+		struct primary_directory_block_entry *entry = &table->entry[i];
 		if (debug)
-			hexdata("primary_directory_block_entry", table->table[i].raw, sizeof(table->table[i].raw));
-		if (!table->table[i].entry.name[0])
+			hexdata("primary_directory_block_entry", entry, sizeof(*entry));
+		if (!entry->name[0])
 			continue;
-		struct primary_directory_block_entry *entry = &table->table[i].entry;
-		print_primary_directory_block_entry(set, entry);
+		print_primary_directory_block_entry(catalogue, entry);
+		int type = entry->attributes & 0xf;
 		if (output_folder) {
-			if (strlen(set)) // Not sure this is right condition. Maybe should look at the extra data after the filename instead.
-				save_file(in, set, entry);
-			else
-				save_index_file(in, set, entry);
+			switch(type) {
+			case FILE_TYPE_CONTIGUOUS:
+				save_file(in, catalogue, entry);
+				break;
+			case FILE_TYPE_SEQUENTIAL:
+				fprintf(stderr, "ERROR: Saving of sequential files not implemented yet");
+				break;
+			case FILE_TYPE_ISAM:
+			case FILE_TYPE_ISAMD:
+				fprintf(stderr, "ERROR: Saving of ISAM files not implemented yet");
+				break;
+			}
 		}
 	}
+	// TODO: Chain to next
 }
 
 void read_secondary_directory_block(FILE *in, uint32_t start_block)
@@ -221,13 +256,14 @@ void read_secondary_directory_block(FILE *in, uint32_t start_block)
 	char buf[0x100];
 	read_at(in, start_block, buf, sizeof(buf));
 	struct secondary_directory_block *table = (void *)buf;
-	//hexdata("block", buf, 0x100);
-	if (verbose)
-		hexdata("unknown", table->unknown1, sizeof(table->unknown1));
-	for (int i = 0; i < 50 && table->table[i].entry.name[0]; i++) {
+	if (debug)
+		hexdata("sdp", buf, sizeof(buf));
+	for (int i = 0; i < 15; i++) {
+		struct secondary_directory_block_entry *entry = &table->entry[i];
 		if (debug)
-			hexdata("secondary_directory_block_entry", table->table[i].raw, sizeof(table->table[i].raw));
-		struct secondary_directory_block_entry *entry = &table->table[i].entry;
+			hexdata("secondary_directory_block_entry", entry, sizeof(*entry));
+		if (!entry->name[0])
+			continue;
 		print_secondary_directory_block_entry(entry);
 		char set_name[9];
 		memcpy(set_name, entry->name, sizeof(entry->name));
@@ -235,6 +271,7 @@ void read_secondary_directory_block(FILE *in, uint32_t start_block)
 		trim_spaces(set_name);
 		read_primary_directory_block(in, set_name, be32toh(entry->block));
 	}
+	// TODO: Chain to next
 }
 
 void print_volume_id(struct volume_id_block *vid)
